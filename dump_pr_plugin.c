7da6eb80c7fc7d5a1e233f5d4f9c449bfc8aa6f7
Hal Rosenstock <hal@mellanox.com>
2010-10-22 17:13:58 +0200

    dump_pr_plugin.c: Initial changes for torus2QoS
    Added support for switch to switch file and peer file when torus2QoS routing
    
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- dump_pr_plugin.c
+++ dump_pr_plugin.c
@@ -51,6 +51,8 @@
 #include <opensm/osm_log.h>
 
 #define DUMP_PR_FILENAME "opensm-path-records.dump"
+#define DUMP_PEER_FILENAME "opensm-peer-paths.dump"
+#define DUMP_SW2SW_FILENAME "opensm-sw2sw-path-records.dump"
 
 typedef struct _path_parms {
 	ib_net16_t pkey;
@@ -70,7 +72,7 @@ osm_get_path_params(IN osm_sa_t * sa,
 
 /*****************************************************************************/
 static FILE *
-open_file(osm_opensm_t *p_osm, const char *file_name)
+open_file(osm_opensm_t * p_osm, const char * file_name)
 {
 	char path[1024];
 	FILE *file;
@@ -106,23 +108,124 @@ close_file(FILE * file)
 }
 
 /*****************************************************************************/
-static void dump_path_records(osm_opensm_t *p_osm)
+static void get_peer_sls(osm_opensm_t * p_osm, FILE * file,
+			 uint16_t sw_dlid_ho, path_parms_t * sw2sw_path_parms,
+			 osm_switch_t * p_src_sw, osm_switch_t * p_dest_sw)
+{
+	osm_physp_t *p_src_physp, *p_dest_physp, *p_src_rem_physp, *p_dest_rem_physp;
+	osm_node_t *p_src_rem_node, *p_dest_rem_node;
+	osm_port_t *p_src_port, *p_dest_port;
+	path_parms_t path_parms;
+	ib_api_status_t status;
+	int num_sls = 0;
+	uint8_t last_sl = 0xff;
+	uint8_t src_port_num, dest_port_num;
+
+	for (src_port_num = 0; src_port_num < p_src_sw->num_ports;
+	     src_port_num++) {
+		p_src_physp = osm_node_get_physp_ptr(p_src_sw->p_node,
+						     src_port_num);
+		p_src_rem_physp = osm_physp_get_remote(p_src_physp);
+		if (!p_src_rem_physp)
+			continue;
+		p_src_rem_node = osm_physp_get_node_ptr(p_src_rem_physp);
+		if (p_src_rem_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+			continue;
+
+		for (dest_port_num = 0; dest_port_num < p_dest_sw->num_ports;
+		     dest_port_num++) {
+			p_dest_physp = osm_node_get_physp_ptr(p_dest_sw->p_node,
+							      dest_port_num);
+			p_dest_rem_physp = osm_physp_get_remote(p_dest_physp);
+			if (!p_dest_rem_physp)
+				continue;
+			p_dest_rem_node = osm_physp_get_node_ptr(p_dest_rem_physp);
+			if (p_dest_rem_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+				continue;
+
+			p_src_port = osm_get_port_by_guid(&p_osm->subn,
+							  p_src_rem_physp->port_guid);
+			p_dest_port = osm_get_port_by_guid(&p_osm->subn,
+							   p_dest_rem_physp->port_guid);
+			CL_ASSERT(p_src_port);
+			CL_ASSERT(p_dest_port);
+
+			status = osm_get_path_params(&p_osm->sa,
+						     p_src_port, p_dest_port,
+						     cl_ntoh16(osm_physp_get_base_lid(p_dest_rem_physp)),
+						     (void *)&path_parms);
+
+			if (!status && path_parms.sl != last_sl) {
+				fprintf(file, "0x%04X : %-2d : %-3d : %-4d\n",
+					sw_dlid_ho, path_parms.sl,
+					sw2sw_path_parms->mtu,
+					sw2sw_path_parms->rate);
+				/* At most 2 different SLs (qos-levels) */
+				if (++num_sls == 2)
+					return;
+			}
+		}
+	}
+
+	if (!num_sls)
+		fprintf(file, "0x%04X : %-2d : %-3d : %-4d\n",
+			sw_dlid_ho, sw2sw_path_parms->sl,
+			sw2sw_path_parms->mtu, sw2sw_path_parms->rate);
+}
+
+/*****************************************************************************/
+static void dump_path_records(osm_opensm_t * p_osm)
 {
 	osm_port_t *p_src_port, *p_dest_port;
 	osm_node_t *p_node;
 	uint16_t dlid_ho;
 	size_t vector_size;
 	osm_physp_t *p_physp;
+	char *full_pr_dump;
+	int is_full_pr_dump, is_opt_pr_dump;
+	FILE *file = NULL, *file2 = NULL, *file3 = NULL;
 	path_parms_t path_parms;
 	ib_api_status_t status;
 
 	OSM_LOG_ENTER(&p_osm->log);
 
-	FILE * file = open_file(p_osm, DUMP_PR_FILENAME);
-	if (!file) {
-		OSM_LOG(&p_osm->log, OSM_LOG_ERROR, "ERR PR02: "
-			"Dumping PR file failed - couldn't open dump file\n");
+	if (p_osm->routing_engine_used &&
+	    p_osm->routing_engine_used->type == OSM_ROUTING_ENGINE_TYPE_TORUS_2QOS)
+		is_opt_pr_dump = 1;
+	else
+		is_opt_pr_dump = 0;
+
+	full_pr_dump = getenv("DUMP_FULL_PATH_RECORDS");
+	if (!full_pr_dump || (*full_pr_dump == '0'))
+		is_full_pr_dump = 0;
+	else
+		is_full_pr_dump = 1;
+
+	if (!is_opt_pr_dump && !is_full_pr_dump)
 		goto Exit;
+
+	if (is_full_pr_dump) {
+		file = open_file(p_osm, DUMP_PR_FILENAME);
+		if (!file) {
+			OSM_LOG(&p_osm->log, OSM_LOG_ERROR, "ERR PR02: "
+				"Dumping PR file failed - couldn't open dump file\n");
+			goto Exit;
+		}
+	}
+
+	if (is_opt_pr_dump) {
+		file2 = open_file(p_osm, DUMP_PEER_FILENAME);
+		if (!file2) {
+			OSM_LOG(&p_osm->log, OSM_LOG_ERROR, "ERR PR03: "
+				"Dumping PR file failed - couldn't open peer dump file\n");
+			goto Exit;
+		}
+		file3 = open_file(p_osm, DUMP_SW2SW_FILENAME);
+		if (!file3) {
+			OSM_LOG(&p_osm->log, OSM_LOG_ERROR, "ERR PR04L "
+				"Dumping PR file failed - couldn't open switch to switch dump file\n");
+			goto Exit;
+		}
 	}
 
 	vector_size = cl_ptr_vector_get_size(&p_osm->subn.port_lid_tbl);
@@ -131,47 +234,98 @@ static void dump_path_records(osm_opensm_t *p_osm)
 	     p_src_port = (osm_port_t *) cl_qmap_next(&p_src_port->map_item)) {
 
 		p_node = p_src_port->p_node;
-		if (p_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+		if (p_node->node_info.node_type == IB_NODE_TYPE_SWITCH &&
+		    !is_opt_pr_dump)
 			continue;
 
 		p_physp = p_src_port->p_physp;
 		CL_ASSERT(p_physp->p_remote_physp);
 
-		fprintf(file, "%s 0x%016" PRIx64 ", base LID %d, "
-			"\"%s\", port %d\n# LID  : SL : MTU : RATE\n",
-			ib_get_node_type_str(p_node->node_info.node_type),
-			cl_ntoh64(p_src_port->guid),
-			cl_ntoh16(osm_port_get_base_lid(p_src_port)),
-			p_node->print_desc, p_physp->port_num);
-
-		memset(&path_parms, 0, sizeof(path_parms_t));
+		if (file && p_node->node_info.node_type != IB_NODE_TYPE_SWITCH)
+			fprintf(file, "%s 0x%016" PRIx64 ", base LID %d, "
+				"\"%s\", port %d\n# LID  : SL : MTU : RATE\n",
+				ib_get_node_type_str(p_node->node_info.node_type),
+				cl_ntoh64(p_src_port->guid),
+				cl_ntoh16(osm_port_get_base_lid(p_src_port)),
+				p_node->print_desc, p_physp->port_num);
+		if (file2 && p_node->node_info.node_type != IB_NODE_TYPE_SWITCH)
+			fprintf(file2, "%s 0x%016" PRIx64 ", base LID %d, "
+				"\"%s\", port %d\n# LID  : MTU : RATE\n",
+				ib_get_node_type_str(p_node->node_info.node_type),
+				cl_ntoh64(p_src_port->guid),
+				cl_ntoh16(osm_port_get_base_lid(p_src_port)),
+				p_node->print_desc, p_physp->port_num);
+		if (file3 &&
+		    p_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+			fprintf(file3, "%s 0x%016" PRIx64 ", base LID %d, "
+				"\"%s\", port %d\n# LID  : SL : MTU : RATE\n",
+				ib_get_node_type_str(p_node->node_info.node_type),
+				cl_ntoh64(p_src_port->guid),
+				cl_ntoh16(osm_port_get_base_lid(p_src_port)),
+				p_node->print_desc, p_physp->port_num);
 
 		for (dlid_ho = 1; dlid_ho < vector_size; dlid_ho++) {
 
 			p_dest_port = (osm_port_t *) cl_ptr_vector_get(
 				&p_osm->subn.port_lid_tbl, dlid_ho);
 
-			if (!p_dest_port || !p_dest_port->p_node ||
-			    p_dest_port->p_node->node_info.node_type ==
-			    IB_NODE_TYPE_SWITCH)
-			continue;
+			if (!p_dest_port || !p_dest_port->p_node)
+				continue;
+			if (p_dest_port->p_node->node_info.node_type ==
+			    IB_NODE_TYPE_SWITCH && !is_opt_pr_dump)
+				continue;
 
 			status = osm_get_path_params(&p_osm->sa,
 				p_src_port, p_dest_port, dlid_ho,
 				(void *)&path_parms);
 
-			if (!status)
-				fprintf(file, "0x%04X : %-2d : %-3d : %-4d\n",
-					dlid_ho, path_parms.sl,
-					path_parms.mtu, path_parms.rate);
-			else
-				fprintf(file, "0x%04X : UNREACHABLE\n",
-					dlid_ho);
+			if (!status) {
+				if (file &&
+				    p_node->node_info.node_type != IB_NODE_TYPE_SWITCH)
+					fprintf(file, "0x%04X : %-2d : %-3d : %-4d\n",
+						dlid_ho, path_parms.sl,
+						path_parms.mtu, path_parms.rate);
+				if (file2 &&
+				    p_node->node_info.node_type != IB_NODE_TYPE_SWITCH &&
+				    p_physp->p_remote_physp->p_node == p_dest_port->p_node)
+					fprintf(file2, "0x%04X : %-3d : %-4d\n\n",
+						dlid_ho, path_parms.mtu,
+						path_parms.rate);
+				if (file3 &&
+				    p_node->node_info.node_type == IB_NODE_TYPE_SWITCH &&
+				    p_dest_port->p_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+					get_peer_sls(p_osm, file3,
+						     dlid_ho, &path_parms,
+						     p_node->sw,
+						     p_dest_port->p_node->sw);
+			} else {
+				if (file &&
+				    p_node->node_info.node_type != IB_NODE_TYPE_SWITCH)
+					fprintf(file, "0x%04X : UNREACHABLE\n",
+						dlid_ho);
+				if (file2 &&
+                                    p_node->node_info.node_type != IB_NODE_TYPE_SWITCH &&
+                                    p_physp->p_remote_physp->p_node == p_dest_port->p_node)
+					fprintf(file2, "0x%04X : UNREACHABLE\n",
+						dlid_ho);
+				if (file3 &&
+				    p_node->node_info.node_type == IB_NODE_TYPE_SWITCH &&
+				    p_dest_port->p_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+					fprintf(file3, "0x%04X : UNREACHABLE\n",
+						dlid_ho);
+			}
 		}
-		fprintf(file, "\n");
+		if (file)
+			fprintf(file, "\n");
+		if (file3 &&
+		    p_node->node_info.node_type == IB_NODE_TYPE_SWITCH)
+			fprintf(file3, "\n");
 	}
-	close_file(file);
+
 Exit:
+	close_file(file3);
+	close_file(file2);
+	close_file(file);
 	OSM_LOG_EXIT(&p_osm->log);
 }
 
